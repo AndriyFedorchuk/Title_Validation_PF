3fc42d0a951e361ef3534898ecd733da9cd3bcdf
AndriyFedorchuk <andriy@flatterthanearth.com>
2015-08-23 14:31:30 -0700

    Hoemwork 35

---
+++ src/main/java/core/Celenium.java
@@ -0,0 +1,60 @@
+package core;
+
+//BEGIN
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Celenium {
+	public static void main(String[] args) {
+
+		
+
+		String text_case_id = null;
+		String url = null;
+		String title_expected = null;
+		Properties Test = new Properties();
+		
+		try {
+			Test.load(new FileInputStream("/Users/paskyda/Workspase/Title_Validation_PF/src/main/resources/Test.properties"));
+			text_case_id = Test.getProperty("text_case_id");
+			url = Test.getProperty("url");
+			title_expected = Test.getProperty("title_expected");
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
+		
+		
+		WebDriver driver = new FirefoxDriver();    // Version 1.1 :: Firefox
+		driver.get(url);
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		String title_actual = driver.getTitle();
+
+		if (title_expected.equals(title_actual)) {
+			System.out.println("Test Case ID: \t\t" + text_case_id);
+			System.out.println("URL: \t\t\t" + url);
+			System.out.println("Title Expected: \t" + title_expected);
+			System.out.println("Title Actual: \t\t" + title_actual);
+			System.out.println("Test Case Result: \t" + "PASSED");
+		} else {
+			System.out.println("Test Case ID: \t\t" + text_case_id);
+			System.out.println("URL: \t\t\t" + url);
+			System.out.println("Title Expected: \t" + title_expected);
+			System.out.println("Title Actual: \t\t" + title_actual);
+			System.out.println("Test Case Result: \t" + "FAILED");
+		}
+		driver.quit();
+	}
+}
+//END
